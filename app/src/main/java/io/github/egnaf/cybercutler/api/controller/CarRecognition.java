cb8a32c83da440260c75e0667b58d0a589b7a630
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-10-11 10:49:27 +0300

    Add endpoints to work with api

--- app/src/main/java/io/github/egnaf/cybercutler/api/controller/CarRecognition.java
+++ app/src/main/java/io/github/egnaf/cybercutler/api/controller/CarRecognition.java
@@ -1,4 +1,14 @@
 package io.github.egnaf.cybercutler.api.controller;
 
+import java.util.Map;
+
+import io.github.egnaf.cybercutler.api.model.CalculatorRequest;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface CarRecognition {
+
+    @POST("/carRecognition")
+    CalculatorRequest carRecognition(@Query("x-ibm-client-id") String clientId, @Body Map<String, Object> body);
 }
--- app/src/main/java/io/github/egnaf/cybercutler/api/controller/Marketplace.java
+++ app/src/main/java/io/github/egnaf/cybercutler/api/controller/Marketplace.java
@@ -1,4 +1,11 @@
 package io.github.egnaf.cybercutler.api.controller;
 
+import io.github.egnaf.cybercutler.api.model.CalculatorRequest;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface Marketplace {
+
+    @GET("/marketplace")
+    CalculatorRequest marketplace(@Query("x-ibm-client-id") String clientId);
 }
